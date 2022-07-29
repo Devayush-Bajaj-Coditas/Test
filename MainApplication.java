529034e7d968044c97d17ad161fbe5c992abdb38
Devayush-Bajaj-Coditas <110122660+Devayush-Bajaj-Coditas@users.noreply.github.com>
2022-07-29 07:10:14 +0100

    Add files via upload

---
+++ MainApplication.java
@@ -0,0 +1,36 @@
+//Q1create an array of size 5 of int and iterate it. ***Find the greatest value in that array
+//Q3
+
+
+import java.util.Arrays;
+
+public class MainApplication {
+    void myArray() {
+        int large = 0;
+        int MYArray[] = {2, 15, 1, 100, 70};
+    //Printing array using for each loop
+        for (int k : MYArray) {
+            System.out.println(k);
+
+            for (int j = 0; j < MYArray.length; j++) {
+                if (k < MYArray[j]) {
+                    large = MYArray[j];
+                }
+            }
+        }
+        System.out.println("Largest value in array is "+ large);
+
+        Arrays.sort(MYArray);
+        System.out.println("Third largest is   " + MYArray[2]);
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        MainApplication mainApplication = new MainApplication();
+        mainApplication.myArray();
+    }
+}
